0076917142098ac70a906f6f2b5841c92decae0f
saladNights <the.withered.fern@gmail.com>
2020-07-04 13:49:17 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          There will be ADA Widget
         </a>
       </header>
     </div>
