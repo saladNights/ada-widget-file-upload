fb70cd74ec66eb31fe33d8371dbc857c960b66a9
saladNights <the.withered.fern@gmail.com>
2020-07-04 15:59:12 +0300

    btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          There will be ADA Widget
-        </a>
-      </header>
-    </div>
-  );
+const AdaWidgetSDK = require("@ada-support/ada-widget-sdk");
+const widgetSDK = new AdaWidgetSDK();
+
+class App extends React.Component<{}, {}> {
+  componentDidMount() {
+    widgetSDK.init((event: any) => {
+      console.log(event);
+      // Perform additional setup steps in here.
+      // event will be one of `WIDGET_INITIALIZED` or `WIDGET_INITIALIZATION_FAILED`
+    });
+  }
+
+  onChangeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    console.log(e.currentTarget.files && e.currentTarget.files[0]);
+  };
+
+  render() {
+    return (
+      <div>
+        <label htmlFor='file' className='custom-file-upload'>
+          <input type='file' name='file' id='file' onChange={this.onChangeHandler}/>
+          Upload File
+        </label>
+      </div>
+    );
+  }
 }
 
 export default App;
