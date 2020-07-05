80828f17ed2e4674e7a3e2eed3e1b9b40164fd6f
saladNights <the.withered.fern@gmail.com>
2020-07-05 18:35:43 +0300

    change config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const apiUrl = 'https://hidden-everglades-39585.herokuapp.com';
-export const apiUrl = 'http://localhost:8000';
+export const apiUrl = 'https://hidden-everglades-39585.herokuapp.com';
+// export const apiUrl = 'http://localhost:8000';
