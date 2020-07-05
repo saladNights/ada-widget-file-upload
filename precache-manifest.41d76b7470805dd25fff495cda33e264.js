cb74fd2f720166b1fff2acbdf4536f9527f5c1f3
saladNights <the.withered.fern@gmail.com>
2020-07-05 12:42:56 +0300

    Updates

---
+++ precache-manifest.41d76b7470805dd25fff495cda33e264.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3d211502eca3bd76da2f060e2d1d0c",
+    "url": "/ada-widget-file-upload/index.html"
+  },
+  {
+    "revision": "90711b9f5b53abbd8991",
+    "url": "/ada-widget-file-upload/static/css/main.f4afbd2d.chunk.css"
+  },
+  {
+    "revision": "8d32857b15e292d3d9dc",
+    "url": "/ada-widget-file-upload/static/js/2.c05ab057.chunk.js"
+  },
+  {
+    "revision": "98c87275776474e5a317cb0df95722c7",
+    "url": "/ada-widget-file-upload/static/js/2.c05ab057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90711b9f5b53abbd8991",
+    "url": "/ada-widget-file-upload/static/js/main.08c804f5.chunk.js"
+  },
+  {
+    "revision": "d0147b5963e6b55ecd9e",
+    "url": "/ada-widget-file-upload/static/js/runtime-main.c3610326.js"
+  }
+]);
